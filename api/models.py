7ae36e85ffb3d20a24755bd4e48f7042e51fe999
Shunya Nagashima <syun864297531@gmail.com>
2022-08-17 20:36:11 +0900

    GeoDjango対応

--- api/models.py
+++ api/models.py
@@ -1,9 +1,11 @@
-from django.db import models
+from django.conf import settings
+from django.contrib.gis.db import models
+User = settings.AUTH_USER_MODEL
 
 class Calendar(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
 
 class Location(models.Model):
-    calendar = models.ForeignKey('Calendar', on_delete=models.CASCADE)
-    # location = 
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+    location = models.PointField(null=True, blank=True)
