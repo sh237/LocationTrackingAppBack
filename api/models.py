088609781034e7f9739a733aab41004e5fc0c621
Shunya Nagashima <syun864297531@gmail.com>
2022-08-17 19:45:53 +0900

    デル追加

--- api/models.py
+++ api/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class Calendar(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    date = models.DateField()
+
+class Location(models.Model):
+    calendar = models.ForeignKey('Calendar', on_delete=models.CASCADE)
+    # location = 
