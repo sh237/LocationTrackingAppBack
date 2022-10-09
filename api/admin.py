d9fdd962d5470767cdb5c76394e139451bb3481d
Shunya Nagashima <syun864297531@gmail.com>
2022-10-09 22:58:23 +0900

    アドミンサイトの設定変更

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from django.contrib.gis import admin as geoadmin                                                                                         
 from . import models
 admin.site.register(models.Location, geoadmin.OSMGeoAdmin)
+# admin.site.register(models.Location)
 admin.site.register(models.Calendar)
+admin.site.register(models.Photo)
